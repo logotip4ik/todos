4d72ef76856587824d2fd11311a5272c523c2d04
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-25 23:44:51 +0300

    update: added basic layout for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import useUser from './hooks/useUser';
+
+import Form from './components/Form';
+
 function App() {
-  return <div className="App"></div>;
+  const { user, loginUser, isUserLoading } = useUser();
+
+  return (
+    <>
+      {isUserLoading && !user ? (
+        <h1>Loading...</h1>
+      ) : !isUserLoading && !user ? (
+        <Form onLogin={loginUser}></Form>
+      ) : (
+        <div>This will be main todo app</div>
+      )}
+    </>
+  );
 }
 
 export default App;
