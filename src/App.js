f18ab602c46bf42f6d33347de90385f9efa58e05
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-28 23:17:41 +0300

    update: new basic layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import useUser from './hooks/useUser';
+import constants from './constants';
 
 import Form from './components/Form';
 import Loader from './components/Loader';
+import Header from './components/Utils/Header';
+import TodoList from './components/Utils/TodoList';
+import BottomBar from './components/Utils/BottomBar';
+
+import Creating from './components/Pages/Create';
 
 const exit = { opacity: 0 };
 const anim = { opacity: 1 };
 
 function App() {
-  const { user, loginUser, isLoggedIn, isUserLoading } = useUser();
+  const [appState, setAppState] = useState(constants.IDLE);
+  const [rawTodos, setRawTodos] = useState(new Set());
+  const { gunUser, loginUser, isLoggedIn, isUserLoading } = useUser();
+
+  useEffect(() => {
+    gunUser
+      .get('todos')
+      .map()
+      .on((data) => setRawTodos((todos) => new Set([...todos, data])));
+  }, []);
+
+  useEffect(() => {
+    console.log(rawTodos);
+  }, [rawTodos]);
 
   return (
-    <AnimatePresence exitBeforeEnter={true} initial={exit}>
-      {isUserLoading ? (
-        <motion.div initial={exit} animate={anim} exit={exit} key={1}>
-          <Loader />
-        </motion.div>
-      ) : !isLoggedIn ? (
-        <motion.div initial={exit} animate={anim} exit={exit} key={2}>
-          <Form onLogin={loginUser}></Form>
-        </motion.div>
-      ) : (
-        <motion.div initial={exit} animate={anim} exit={exit} key={3}>
-          This will be main todo app
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <>
+      <AnimatePresence exitBeforeEnter={true} initial={exit}>
+        {isUserLoading ? (
+          <motion.div initial={exit} animate={anim} exit={exit} key={1}>
+            <Loader />
+          </motion.div>
+        ) : !isLoggedIn ? (
+          <motion.div
+            initial={exit}
+            animate={anim}
+            exit={exit}
+            key={2}
+            style={{ height: '100%' }}
+          >
+            <Form onLogin={loginUser}></Form>
+          </motion.div>
+        ) : (
+          <motion.div initial={exit} animate={anim} exit={exit} key={3}>
+            <Header />
+            <TodoList rawTodos={rawTodos} />
+            <BottomBar
+              appState={appState}
+              onClickCreate={(state) => setAppState(state)}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <Creating isOpened={appState === constants.CREATING} />
+    </>
   );
 }
 
