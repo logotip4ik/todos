f7758ef1651628126d72a6595bc6f36137a3f45b
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-28 09:48:31 +0300

    update: now main transition is working properly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import useUser from './hooks/useUser';
 
 import Form from './components/Form';
+import Loader from './components/Loader';
 
 const exit = { opacity: 0 };
 const anim = { opacity: 1 };
@@ -10,17 +11,17 @@ function App() {
   const { user, loginUser, isLoggedIn, isUserLoading } = useUser();
 
   return (
-    <AnimatePresence exitBeforeEnter={true} initial={false}>
+    <AnimatePresence exitBeforeEnter={true} initial={exit}>
       {isUserLoading ? (
-        <motion.h1 initial={exit} animate={anim} exit={exit}>
-          Loading...
-        </motion.h1>
+        <motion.div initial={exit} animate={anim} exit={exit} key={1}>
+          <Loader />
+        </motion.div>
       ) : !isLoggedIn ? (
-        <motion.div initial={exit} animate={anim} exit={exit}>
+        <motion.div initial={exit} animate={anim} exit={exit} key={2}>
           <Form onLogin={loginUser}></Form>
         </motion.div>
       ) : (
-        <motion.div initial={exit} animate={anim} exit={exit}>
+        <motion.div initial={exit} animate={anim} exit={exit} key={3}>
           This will be main todo app
         </motion.div>
       )}
