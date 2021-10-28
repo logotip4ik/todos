a63d02d6e610136c09219a158a3448fec070eef2
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-28 08:59:49 +0300

    update: added kinda layout of the app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import useUser from './hooks/useUser';
 
 import Form from './components/Form';
 
+const exit = { opacity: 0 };
+const anim = { opacity: 1 };
+
 function App() {
-  const { user, loginUser, isUserLoading } = useUser();
+  const { user, loginUser, isLoggedIn, isUserLoading } = useUser();
 
   return (
-    <>
-      {isUserLoading && !user ? (
-        <h1>Loading...</h1>
-      ) : !isUserLoading && !user ? (
-        <Form onLogin={loginUser}></Form>
+    <AnimatePresence exitBeforeEnter={true} initial={false}>
+      {isUserLoading ? (
+        <motion.h1 initial={exit} animate={anim} exit={exit}>
+          Loading...
+        </motion.h1>
+      ) : !isLoggedIn ? (
+        <motion.div initial={exit} animate={anim} exit={exit}>
+          <Form onLogin={loginUser}></Form>
+        </motion.div>
       ) : (
-        <div>This will be main todo app</div>
+        <motion.div initial={exit} animate={anim} exit={exit}>
+          This will be main todo app
+        </motion.div>
       )}
-    </>
+    </AnimatePresence>
   );
 }
 
