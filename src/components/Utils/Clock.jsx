8ee120a9bc20334b75c3cfd12bcf3ae987a7dcd9
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-31 23:12:21 +0200

    update: added clock component

---
+++ src/components/Utils/Clock.jsx
@@ -0,0 +1,59 @@
+import styles from '../../styles/Utils/Clock.module.scss';
+
+import {
+  AnimatePresence,
+  motion,
+  useTransform,
+  useViewportScroll,
+} from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+const numberVariants = {
+  initial: { y: '-150%' },
+  animate: { y: '0%' },
+  exit: { y: '150%' },
+};
+
+const getTime = () =>
+  Intl.DateTimeFormat('en', {
+    hour: '2-digit',
+    hourCycle: 'h23',
+    minute: '2-digit',
+  }).format(new Date());
+
+const xOffset = 14;
+const breakpoint = 100;
+
+function Clock() {
+  const { scrollY } = useViewportScroll();
+  const top = useTransform(scrollY, [0, breakpoint], [91, 14]);
+  const [time, setTime] = useState(getTime());
+
+  useEffect(() => setInterval(() => setTime(getTime()), 1000), []);
+
+  return (
+    <motion.button
+      className={styles.clock}
+      style={{ width: 81, height: 45, top }}
+    >
+      <AnimatePresence>
+        {time.split('').map((val, i) => {
+          return (
+            <motion.span
+              style={{ width: xOffset }}
+              initial={{ top: '-120%', left: i * xOffset + 5 }}
+              animate={{ top: '15%', left: i * xOffset + 5 }}
+              exit={{ top: '120%', left: i * xOffset + 5 }}
+              key={val + i}
+              className={styles.clock__number}
+            >
+              {val}
+            </motion.span>
+          );
+        })}
+      </AnimatePresence>
+    </motion.button>
+  );
+}
+
+export default Clock;
