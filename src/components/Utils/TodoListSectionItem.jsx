610b948dc4c9a3513d7181bcef612d7930297ffe
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-31 15:46:55 +0200

    update: added function for completing todo

--- src/components/Utils/TodoListSectionItem.jsx
+++ src/components/Utils/TodoListSectionItem.jsx
@@ -1,17 +1,36 @@
 import styles from '../../styles/Utils/TodoListSectionItem.module.scss';
 
+import { useCallback } from 'react';
 import { motion } from 'framer-motion';
+import useUser from '../../hooks/useUser';
 
 function TodoListSectionItem({ todo }) {
+  const { gunUser } = useUser();
+
+  const toggleComplete = useCallback(() => {
+    gunUser().get('todos').get(todo.id).get('completed').put(!todo.completed);
+  }, [todo, gunUser]);
+
   return (
-    <motion.li layout className={styles.todo}>
+    <motion.li
+      layout
+      className={`${styles.todo} ${
+        todo.completed ? styles['todo--completed'] : ''
+      }`}
+    >
       <button
         className={`${styles.todo__button} ${
           todo.completed ? styles['todo__button--completed'] : ''
         }`}
-        onClick={() => {}}
+        onClick={toggleComplete}
       ></button>
-      <p className={styles.todo__text}>{todo.data}</p>
+      <p
+        className={`${styles.todo__text} ${
+          todo.completed ? styles['todo__text--completed'] : ''
+        }`}
+      >
+        {todo.data}
+      </p>
     </motion.li>
   );
 }
