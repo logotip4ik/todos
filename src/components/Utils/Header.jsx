e93192cbf52d1be8b84f3b63f7ed30a0749a98e0
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-31 14:41:08 +0200

    update: added animation to header

--- src/components/Utils/Header.jsx
+++ src/components/Utils/Header.jsx
@@ -1,10 +1,39 @@
 import styles from '../../styles/Utils/Header.module.scss';
 
+import { useViewportScroll, useTransform, motion } from 'framer-motion';
+
+const breakpoint = 200;
+
 function Header() {
+  const { scrollY } = useViewportScroll();
+
+  const paddingTop = useTransform(scrollY, [0, breakpoint], [73.5, 12]);
+  const basePadding = useTransform(scrollY, [0, breakpoint], [28, 12]);
+  const baseScale = useTransform(scrollY, [0, breakpoint], [1, 0.5]);
+
+  // idk, why but font size wont animate, so i used line height and scale
+
   return (
-    <header className={styles.header}>
-      <h1 className={styles.header__title}>Todo</h1>
-    </header>
+    <motion.header
+      className={styles.header}
+      style={{
+        paddingTop,
+        paddingLeft: basePadding,
+        paddingRight: basePadding,
+        paddingBottom: basePadding,
+      }}
+    >
+      <motion.h1
+        className={styles.header__title}
+        style={{
+          scale: baseScale,
+          lineHeight: baseScale,
+          transformOrigin: 'left center',
+        }}
+      >
+        Todo
+      </motion.h1>
+    </motion.header>
   );
 }
 
